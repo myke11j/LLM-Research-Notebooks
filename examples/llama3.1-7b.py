ddfa504d7affd78f50bc8722ec04fd090875986d
Mukul Jain <12976433+myke11j@users.noreply.github.com>
2024-10-14 23:40:19 +0700

    Update llama3.1-7b.py

--- examples/llama3.1-7b.py
+++ examples/llama3.1-7b.py
@@ -1,1 +1,15 @@
+import re
 import torch
+from langchain import HuggingFacePipeline, PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+from transformers import AutoTokenizer, TextStreamer, pipeline, BitsAndBytesConfig, AutoModelForCausalLM
+import time
+import requests
+import gradio as gr
+import os
+
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
