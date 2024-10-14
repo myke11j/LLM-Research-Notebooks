01482f476cc749b21272fd289a7206f4a2ca5d00
Mukul Jain <12976433+myke11j@users.noreply.github.com>
2024-10-14 23:41:24 +0700

    Update llama3.1-7b.py

--- examples/llama3.1-7b.py
+++ examples/llama3.1-7b.py
@@ -13,3 +13,21 @@ import gradio as gr
 import os
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+model_name = "meta-llama/Llama-3.1-8B-Instruct"
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    token=HUGGING_FACE_TOKEN,
+    quantization_config=bnb_config
+)
+
+
+embeddings = HuggingFaceInstructEmbeddings(
+    model_name="hkunlp/instructor-large",
+    model_kwargs={"device": DEVICE}
+)
